8b4839589c1959123f7f6fa0a45a2892fd8ce402
java4fun <perl4fun@gmail.com>
2021-12-09 09:12:16 -0800

    Promise.all

--- async/ajax.js
+++ async/ajax.js
@@ -55,21 +55,32 @@ function tempToF(kelvin) {
 document.addEventListener('DOMContentLoaded', function() {
   const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
   //const apiKey = '';
+  const weatherDiv = document.querySelector('#weather');
 
-  const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' +
-    apiKey;
+  const locations = [
+    'los+angeles,us',
+    'san+francisco,us',
+    'lone+pine,us',
+    'mariposa,us'
+  ];
+  const urls = locations.map(function(location) {
+    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+  });
+
   // get(url, successHandler, failHandler);
-  // console.log(get(url));
-  get(url)
-    .then(function(response) {
-      successHandler(response);
+  Promise.all([get(urls[0]), get(urls[1]), get(urls[2]), get(urls[3])])
+    .then(function(responses) {
+      return responses.map(function(response) {
+        return successHandler(response);
+      });
+    })
+    .then(function(literals) {
+      weatherDiv.innerHTML = `<h1>Weather</h1>${literals.join('')}`;
     })
     .catch(function(status) {
       failHandler(status);
     })
     .finally(function() {
-      const weatherDiv = document.querySelector('#weather');
       weatherDiv.classList.remove('hidden');
     });
 });
